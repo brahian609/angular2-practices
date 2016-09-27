a83c397bca6fd6163453371dd83751875cc055cb
brahian609 <brahian609@hotmail.com>
2016-09-27 15:41:59 -0500

    feat(project) Add widgets

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,7 +3,9 @@ import {Component} from "@angular/core";
 @Component({
     selector:'home',
     template: `
-<div>I'm a Home Component</div>
-`
+        <div>I'm a Home Component</div>
+        <widget-one></widget-one>
+        <widget-two></widget-two>
+    `
 })
 export class HomeComponent{}
---
+++ app/widgets/widget-two.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'widget-two',
+    template: `
+    <div>Two</div>
+    `
+})
+export class WidgetTwo{}
