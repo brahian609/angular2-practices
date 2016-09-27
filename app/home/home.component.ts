a24456e161fcb090fdb375fbaab49512ef2b7cfb
brahian609 <brahian609@hotmail.com>
2016-09-27 16:03:41 -0500

    feat(project) Add services

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,11 +1,15 @@
 import {Component} from "@angular/core";
+import {SimpleService} from "../services/services.module";
 
 @Component({
     selector:'home',
     template: `
         <div>I'm a Home Component</div>
-        <widget-one></widget-one>
-        <widget-two></widget-two>
+        <div>{{simpleService.message}}</div>
+        <widget-one [message]="simpleService.message"></widget-one>
+        <widget-one [message]="'Hola mundo'"></widget-one>
     `
 })
-export class HomeComponent{}
+export class HomeComponent{
+    constructor(private simpleService:SimpleService){}
+}
