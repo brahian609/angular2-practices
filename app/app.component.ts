a28aff4e7c1dfce550ee2f5cad5293092ec710c0
brahian609 <brahian609@hotmail.com>
2016-09-27 15:23:32 -0500

    feat(project) structure application

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'app',
+    template: `
+<div>I'm the App Component</div>
+`
+})
+export class AppComponent{}
--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,5 @@
-import {Component, NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-@Component({
-    selector:'app',
-    template: `
-<div>I'm the App Component</div>
-`
-})
-export class AppComponent{}
+import {AppModule} from "./app.module";
 
-@NgModule({
-    imports:[BrowserModule],
-    declarations:[AppComponent],
-    bootstrap:[AppComponent]
-})
-export class AppModule{}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
