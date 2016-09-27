78812ecf3712fa135eda0d3c0238fa96e32c5271
brahian609 <brahian609@hotmail.com>
2016-09-27 15:29:58 -0500

    feat(home) Add new component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
-    imports:[BrowserModule],
+    imports:[BrowserModule, HomeModule],
     declarations:[AppComponent],
     bootstrap:[AppComponent]
 })
