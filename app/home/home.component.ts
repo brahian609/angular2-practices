6a49ec7269c8cbdc71e975379fdae88dd4b7310a
brahian609 <brahian609@hotmail.com>
2016-09-29 10:38:10 -0500

    feat(project) Access DOM Elements

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -4,14 +4,7 @@ import {SimpleService} from "../services/services.module";
 @Component({
     selector:'home',
     template: `
-        <!--<widget-two>
-            <widget-one [message]="simpleService.message"></widget-one>
-        </widget-two>-->
-        <widget-two>
-            <h2 header>Title</h2>
-            <div content>Mas contenido</div>
-            <h3 footer>Footer</h3>
-        </widget-two>
+        <widget-three></widget-three>
     `
 })
 export class HomeComponent{
